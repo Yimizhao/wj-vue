63dc701f0b9e292aad5aeda2444532585ddb7d4f
yimizhao <zhao12051610@163.com>
2022-04-29 14:33:05 +0800

    添加Vuex组件，前端实现登录记忆

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,20 @@
 <template>
   <body id="poster">
-    <el-form class="login-container" label-position="left"
-             label-width="0px">
-      <h3 class="login_title">系统登录</h3>
-      <el-form-item>
-        <el-input type="text" v-model="loginForm.username"
-                  auto-complete="off" placeholder="账号"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input type="password" v-model="loginForm.password"
-                  auto-complete="off" placeholder="密码"></el-input>
-      </el-form-item>
-      <el-form-item style="width: 100%">
-        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
-      </el-form-item>
-    </el-form>
+  <el-form class="login-container" label-position="left"
+           label-width="0px">
+    <h3 class="login_title">系统登录</h3>
+    <el-form-item>
+      <el-input type="text" v-model="loginForm.username"
+                auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" v-model="loginForm.password"
+                auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+    </el-form-item>
+  </el-form>
   </body>
 
 </template>
@@ -34,6 +34,7 @@ export default {
   },
   methods: {
     login () {
+      var _this = this
       this.$axios
         .post('/login', {
           username: this.loginForm.username,
@@ -41,7 +42,10 @@ export default {
         })
         .then(successResponse => {
           if (successResponse.data.code === 200) {
-            this.$router.replace({path: '/index'})
+            // var data = this.loginForm
+            _this.$store.commit('login', _this.loginForm)
+            var path = this.$route.query.redirect
+            this.$router.replace({path: path === '/' || path === undefined ? '/index' : path})
           }
         })
         .catch(failResponse => {
@@ -77,7 +81,7 @@ export default {
   position: fixed;
 }
 
-body,html{
+body, html {
   height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
@@ -12,11 +13,27 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (store.state.user.username) {
+      next()
+    } else {
+      next({
+        path: 'login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next()
+  }
+}
+)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
   router,
-  components: { App },
+  store,
+  components: {App},
   template: '<App/>'
 })
